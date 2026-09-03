8c3c6b560e932d6562a8db7c72eabaaa249795c3
Dorjoo <dorjoohover@gmail.com>
2026-09-03 10:31:45 +0000

    fix: allow custom domains for Next.js Server Actions (reverse proxy origin check)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["tokhiruulga.mn", "www.tokhiruulga.mn", "admin.tokhiruulga.mn"],
+    },
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
